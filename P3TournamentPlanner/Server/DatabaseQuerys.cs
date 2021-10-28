f3f77ceedf217490b1dc66f7fc919a3f93316e43
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-10-28 10:41:54 +0200

    Added SQL Class

---
+++ P3TournamentPlanner/Server/DatabaseQuerys.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace P3TournamentPlanner.Server {
+    public class DatabaseQuerys {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TourneyTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public void RunQuery(string query)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public T SelectQuery<T>(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while(reader.Read())
+                    {
+                        T eksempel = (T)reader["playerName"];
+                        return eksempel;
+                    }   
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return default(T);
+        }
+    }
+}
