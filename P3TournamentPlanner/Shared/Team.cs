f59c2a3ef6bd580cfe9a19800a8cb38b130ffe8b
MartinSvanholm <msvanh20@student.aau.dk>
2021-10-28 13:30:33 +0200

    Dynamic table

---
+++ P3TournamentPlanner/Shared/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3TournamentPlanner.Shared
+{
+    public class Team
+    {
+        public Team(string navn)
+        {
+            Navn = navn;
+        }
+        public string Navn { get; set; }
+    }
+}
