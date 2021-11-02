3ccf3b821343ba70eca3201a2230b84d59065c84
MattSmithThe4th <70754046+MattSmithThe4th@users.noreply.github.com>
2021-11-02 10:22:49 +0100

    Register burde virke nu

--- P3TournamentPlanner/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ P3TournamentPlanner/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -28,13 +28,13 @@ namespace P3TournamentPlanner.Server.Areas.Identity.Pages.Account
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger/*,
+            IEmailSender emailSender*/)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
+            //_emailSender = emailSender;
         }
 
         [BindProperty]
