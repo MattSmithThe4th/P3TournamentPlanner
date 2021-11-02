3dd6a78856cc96ec844fe17e511c0d3d7f34f891
Julian <julian.larsen07@gmail.com>
2021-11-02 12:07:53 +0100

    s

--- P3TournamentPlanner/Server/Controllers/StillingController.cs
+++ P3TournamentPlanner/Server/Controllers/StillingController.cs
@@ -6,21 +6,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using P3TournamentPlanner.Shared;
 using System.Data;
-using Microsoft.AspNetCore.Authorization;
 
 namespace P3TournamentPlanner.Server.Controllers {
     [Route("[controller]")]
     [ApiController]
     public class StillingController : ControllerBase {
 
-        //Runs when a get request is send to /Stilling. It creates a list of teams in the given league and division,
-        //based on data in the database. This list is then send as response, as a json.
-        //[AllowAnonymous]
         [HttpGet]
-        public List<Team> Get(string league, int division) {
+        public List<Team> Get() {
             Console.WriteLine("Get Recieved!");
-            Console.WriteLine("league: " + league);
-            Console.WriteLine("division: " + division);
+            //Console.WriteLine("league: " + league);
+            //Console.WriteLine("division: " + division);
 
             //string league, int division
 
@@ -50,10 +46,9 @@ namespace P3TournamentPlanner.Server.Controllers {
 
             //strArr = db.SelectQuery<string>("select teamName, placement, matchPlayed, matchesWon, matchesDraw, matchesLost, points from TeamsDB where divisionID = 1337", "teamName");
 
-            dt = db.PullTable("select teamName, placement, matchPlayed, matchesWon, matchesDraw, matchesLost, points from TeamsDB where divisionID = " + division + " and leagueID = '" + league + "'");
+            dt = db.PullTable("select teamName, placement, matchPlayed, matchesWon, matchesDraw, matchesLost, points from TeamsDB where divisionID = 1337");
 
-            foreach(DataRow r in dt.Rows)
-            {
+            foreach (DataRow r in dt.Rows) {
                 teamList.Add(new Team(r[0].ToString(), (int)r[1], (int)r[2], (int)r[3], (int)r[4], (int)r[5], (int)r[6]));
             }
 
@@ -64,7 +59,15 @@ namespace P3TournamentPlanner.Server.Controllers {
             foreach (Team t in teamList) {
                 Console.WriteLine(t.name);
             }
+
             return teamList;
         }
+
+
+
+
+
+
+
     }
 }
