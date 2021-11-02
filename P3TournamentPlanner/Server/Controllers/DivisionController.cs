4671e8c2bf23b23b956d508258a28c9bd9fdef49
Julian <julian.larsen07@gmail.com>
2021-11-02 14:21:30 +0100

    Set up DivisionController

---
+++ P3TournamentPlanner/Server/Controllers/DivisionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3TournamentPlanner.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P3TournamentPlanner.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class DivisionController : ControllerBase {
+        public List<Division> Get() {
+            Console.WriteLine("Get Recieved!");
+            //Console.WriteLine("league: " + league);
+            //Console.WriteLine("division: " + division);
+
+            DatabaseQuerys db = new DatabaseQuerys();
+
+            List<Division> divList = new List<Division>();
+            
+
+            DataTable dt;
+            DivisionFormat df = new DivisionFormat();
+
+            
+            dt = db.PullTable("select divisionID, leagueID, divisionFormat from DivisionsDB where divisionID = 1");
+            
+            foreach (DataRow r in dt.Rows) {
+                df.format = r[2].ToString();
+                divList.Add(new Division((int)r[0], r[1].ToString(), df));
+            }
+
+            Console.WriteLine(dt);
+
+            foreach (Division t in divList) {
+                Console.WriteLine(t.DivID);
+            }
+
+            return divList;
+        }
+    }
+}
--- P3TournamentPlanner/Shared/DivisionFormat.cs
+++ P3TournamentPlanner/Shared/DivisionFormat.cs
@@ -1,11 +1,11 @@
 ﻿namespace P3TournamentPlanner.Shared {
     public class DivisionFormat {
+        public string format { get; set; }
 
 
 
 
 
-        
 
 
     }
