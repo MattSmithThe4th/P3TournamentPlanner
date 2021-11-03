1696be73dc933bf68405c5ee614b577e2a051cd3
Kristoffermg <kristoffermoeller151@gmail.com>
2021-11-03 13:34:28 +0100

    TeamInformation controller
    Tilføjet controller til holdside

---
+++ P3TournamentPlanner/Server/Controllers/TeamInformation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using P3TournamentPlanner.Shared;
+using System.Data;
+
+namespace P3TournamentPlanner.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class TeamInformationController : ControllerBase {
+
+        [HttpGet]
+        public List<Team> Get()
+        {
+            Console.WriteLine("Get Received!----------------------------------------------------------------------------->");
+
+            DatabaseQuerys db = new DatabaseQuerys();
+
+            List<Team> teamList = new List<Team>();
+
+            DataTable dt;
+
+            dt = db.PullTable("select teamName, managerTlf, divisionID from TeamsDB");
+
+            foreach (DataRow r in dt.Rows)
+            {
+                teamList.Add(new Team((string)r[0], (string)r[1], (int)r[2]));
+            }
+
+            Console.WriteLine(dt);
+
+            //Construct dem som objecter, og send listen afsted
+
+            return teamList;
+        }
+    }
+}
