934e024cbe64f8f2b976102dc1ebfeb256f69166
MartinSvanholm <msvanh20@student.aau.dk>
2021-11-03 12:25:35 +0100

    division get

--- P3TournamentPlanner/Shared/Division.cs
+++ P3TournamentPlanner/Shared/Division.cs
@@ -2,8 +2,22 @@
 
 namespace P3TournamentPlanner.Shared {
     public class Division {
-        public List<Team> teams;
-        public List<Match> matches;
-        public DivisionFormat format;
+        public List<Team> teams { get; set; }
+        public List<Match> matches { get; set; }
+        public DivisionFormat DivFormat { get; set; }
+        public int DivID { get; set; }
+        public string LeagueID { get; set; }
+
+
+
+        public Division() { 
+        
+        }
+
+        public Division(int divisionID, string leagueID, DivisionFormat divFormat) {
+            DivID = divisionID;
+            LeagueID = leagueID;
+            DivFormat = divFormat;
+        }
     }
 }
