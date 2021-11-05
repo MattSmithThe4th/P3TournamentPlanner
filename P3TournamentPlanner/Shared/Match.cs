244f1eb8e3bb831b0e600da0ecba2cc8e7e9581e
Julian <julian.larsen07@gmail.com>
2021-11-05 23:44:41 +0100

    Revert "Revert "Fixed errors - finished unless more changes are coming""
    This reverts commit 36d6b3f908802be1b627c28fe97963a25dfb59e6.

--- P3TournamentPlanner/Shared/Match.cs
+++ P3TournamentPlanner/Shared/Match.cs
@@ -7,16 +7,17 @@ namespace P3TournamentPlanner.Shared {
         public int leagueID { get; set; }
         public List<Team> teams { get; set; }
         public string startTime { get; set; }
-        public bool playedFlag { get; set; }
+        public int playedFlag { get; set; }
         public string results { get; set; }
         public int clubHostID { get; set; }
         public string serverIP { get; set; }
 
-        public Match(int matchID, int divisionID, int leagueID, List<Team> teams, string startTime, bool playedFlag, int clubHostID, string serverIP) {
+        public Match(int matchID, int divisionID, int leagueID, List<Team> teams, string results, string startTime, int playedFlag, int clubHostID, string serverIP) {
             this.matchID = matchID;
             this.divisionID = divisionID;
             this.leagueID = leagueID;
             this.teams = teams;
+            this.results = results;
             this.startTime = startTime;
             this.playedFlag = playedFlag;
             this.clubHostID = clubHostID;
