11dc7f2c615eef713dceb2025bceb56631414224
MattSmithThe4th <70754046+MattSmithThe4th@users.noreply.github.com>
2021-11-05 10:49:24 +0100

    PlayerController Get function

--- P3TournamentPlanner/Shared/Player.cs
+++ P3TournamentPlanner/Shared/Player.cs
@@ -1,8 +1,20 @@
 ﻿namespace P3TournamentPlanner.Shared {
     public class Player {
-        public string name;
-        public string steamID;
+        public int clubID { get; set; }
+        public int teamID { get; set; }
+        public string IRLName { get; set; }
+        public string IGName { get; set; }
+        public string steamID { get; set; }
+        public string cssgoRank { get; set; }
         public int playerSkllRating;
 
+        public Player(int clubID, int teamID, string IRLName, string IGName, string steamID, string cssgoRank) {
+            this.clubID = clubID;
+            this.teamID = teamID;
+            this.IRLName = IRLName;
+            this.IGName = IGName;
+            this.steamID = steamID;
+            this.cssgoRank = cssgoRank;
+        }
     }
 }
