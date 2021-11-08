fe70653ecef7c4650001e8060bf2fdb519088733
Julian <julian.larsen07@gmail.com>
2021-11-08 14:40:15 +0100

    club controller shit done
    a

---
+++ P3TournamentPlanner/Server/Controllers/KlubController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3TournamentPlanner.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P3TournamentPlanner.Server.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class KlubController : ControllerBase {
+        public List<Club> Get() {
+            Console.WriteLine("Get Recieved!");
+
+            DatabaseQuerys db = new DatabaseQuerys();
+
+            List<Club> clubList = new List<Club>();
+
+            DataTable dt;
+
+            dt = db.PullTable($"select clubID, clubName, clubAddress from ClubDB");
+
+
+            foreach (DataRow r in dt.Rows) {
+                clubList.Add(new Club(r[0].ToString(), r[1].ToString(), r[2].ToString()));
+            }
+
+            Console.WriteLine(clubList);
+
+            return clubList;
+        }
+    }
+}
--- P3TournamentPlanner/Shared/Club.cs
+++ P3TournamentPlanner/Shared/Club.cs
@@ -2,16 +2,24 @@
 
 namespace P3TournamentPlanner.Shared {
     public class Club {
+
+        public string clubID { get; set; }
         public string name { get; set; }
         public List<Player> players { get; set; }
         public List<ClubManager> clubManagers { get; set; }
-        public string adress { get; set; }
+        public string address { get; set; }
 
         public Club() {
         }
 
         public Club(string name) {
             this.name = name;
+        }
+
+        public Club(string clubID, string name, string address) {
+            this.clubID = clubID;
+            this.name = name;
+            this.address = address;
         }
     }
 }
