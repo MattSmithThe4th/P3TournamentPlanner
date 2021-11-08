93e6036f19ac93a5c6047adcd6df773bde8670d8
MartinSvanholm <msvanh20@student.aau.dk>
2021-11-08 13:04:27 +0100

    Match controller

---
+++ P3TournamentPlanner/Server/Controllers/MatchController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P3TournamentPlanner.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P3TournamentPlanner.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+        public List<Match> Get()
+        {
+            Console.WriteLine("Get Recieved!");
+
+            //Console.WriteLine("Match ID: " + matchID.GetValueOrDefault());
+
+
+            DatabaseQuerys db = new DatabaseQuerys();
+
+            List<Match> matchList = new List<Match>();
+            List<Team> teamList = new List<Team>();
+
+
+            DataTable matchTable, teamTable;
+
+            matchTable = db.PullTable($"select matchID, divisionID, leagueID, team1ID, team2ID, team1Score, team2Score, " +
+                $"startTime, playedFlag, hostClubID, serverIP from MatchDB");
+
+
+            foreach (DataRow r in matchTable.Rows)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    teamTable = db.PullTable($"select teamID, clubID, divisionID, leagueID, teamName, teamRating, placement, matchPlayed, matchesWon, matchesDraw, " +
+                    $"matchesLost, roundsWon, roundsLost, points, managerID, archiveFlag from TeamsDB where teamID = {(int)r[3 + i]}");
+                    foreach (DataRow row in teamTable.Rows)
+                    {
+                        teamList.Add(new Team((int)row[0], (int)row[1], (int)row[2], (int)row[3], row[4].ToString(), (int)row[5], (int)row[6], (int)row[7], (int)row[8],
+                            (int)row[9], (int)row[10], (int)row[11], (int)row[12], (int)row[13], row[14].ToString(), (int)row[15]));
+                    }
+
+                }
+
+                matchList.Add(new Match((int)r[0], (int)r[1], (int)r[2], teamList, r[5] + " - " + r[6], r[7].ToString(), (int)r[8], (int)r[9], r[10].ToString())); //do something with results
+            }
+
+            Console.WriteLine(matchTable);
+
+            return matchList;
+        }
+    }
+}
