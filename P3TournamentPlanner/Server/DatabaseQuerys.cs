594187d0d8550c980ec419b67a4cc583834c0e0a
Koidesoikz <73647151+Koidesoikz@users.noreply.github.com>
2021-11-12 10:34:00 +0100

    Revert "Update DatabaseQuerys.cs"
    This reverts commit ac99e3acdd1735bf12985e3d41710d340086e268.

--- P3TournamentPlanner/Server/DatabaseQuerys.cs
+++ P3TournamentPlanner/Server/DatabaseQuerys.cs
@@ -1,45 +1,53 @@
 ﻿using Microsoft.Data.SqlClient;
-using System.Collections.Generic;
 using System.Data;
 
 namespace P3TournamentPlanner.Server {
     public class DatabaseQuerys {
         string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GeneralDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public void RunQuery(string query) {
-            using(SqlConnection connection = new SqlConnection(connectionString)) {
+        public void RunQuery(string query)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
-
                 command.ExecuteNonQuery();
                 connection.Close();
             }
         }
 
-        public T SelectQuery<T>(string query, string col) {
-            using(SqlConnection connection = new SqlConnection(connectionString)) {
+        public T SelectQuery<T>(string query, string col)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
                 SqlDataReader reader = command.ExecuteReader();
-                try {
-                    while(reader.Read()) {
+                try
+                {
+                    while(reader.Read())
+                    {
                         T eksempel = (T)reader[col];
                         return eksempel;
-                    }
-                } finally {
+                    }   
+                }
+                finally
+                {
                     connection.Close();
                 }
             }
             return default(T);
         }
 
-        public DataTable PullTable(SqlCommand command) {
+        public DataTable PullTable(string query) {
 
             DataTable table = new DataTable();
 
-            using(SqlConnection connection = new SqlConnection(connectionString)) {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
                 connection.Open();
-                command.Connection = connection;
-                SqlDataAdapter da = new SqlDataAdapter(command);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
                 da.Fill(table);
                 connection.Close();
                 da.Dispose();
@@ -48,9 +56,11 @@ namespace P3TournamentPlanner.Server {
 
         }
 
-        public void InsertToTable(SqlCommand command) {
+        public void InsertToTable(string query) {
             using(SqlConnection connection = new SqlConnection(connectionString)) {
+                SqlCommand cmd = new SqlCommand(query, connection);
                 connection.Open();
+                command.Connection = connection;
                 command.ExecuteNonQuery();
                 connection.Close();
             }
